8d63d3c8ce2afda896bddb955fc9012dcc50caa3
william <william@tcell.io>
2017-05-11 12:40:37 +0200

    Use c_char type instead of u8

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,16 @@ extern crate regex;
 
 use std::slice;
 use std::str;
-use std::os::raw::c_int;
+use std::os::raw::{c_char, c_int};
 use regex::RegexBuilder;
 
 // Matches the value string to the regex pattern, returns 1 for match, 0 for non-match, negative for error
-pub extern fn match_regex(pattern: *const u8, pattern_len: usize, value: *const u8, value_len: usize) -> c_int {
-    let pattern_str = match str::from_utf8(unsafe{slice::from_raw_parts(pattern, pattern_len)}) {
+pub extern fn match_regex(pattern: *const c_char, pattern_len: usize, value: *const c_char, value_len: usize) -> c_int {
+    let pattern_str = match str::from_utf8(unsafe{slice::from_raw_parts(pattern as *const u8, pattern_len)}) {
         Ok(s) => s,
         _ => return -1
     };
-    let value_str = match str::from_utf8(unsafe { slice::from_raw_parts(value, value_len) }) {
+    let value_str = match str::from_utf8(unsafe { slice::from_raw_parts(value as *const u8, value_len) }) {
         Ok(s) => s,
         _ => return -2
     };
@@ -24,11 +24,12 @@ pub extern fn match_regex(pattern: *const u8, pattern_len: usize, value: *const 
 #[cfg(test)]
 mod tests {
     use match_regex;
+    use std::os::raw::c_char;
 
     #[test]
     fn test_regex() {
         let pattern = r"(?:<(script|iframe|embed|frame|frameset|object|img|applet|body|html|style|layer|link|ilayer|meta|bgsound))";
         let value = r"<script>alert(123)</script>";
-        assert_eq!(match_regex(pattern.as_ptr(), pattern.len(), value.as_ptr(), value.len()), 1);
+        assert_eq!(match_regex(pattern.as_ptr() as *const c_char, pattern.len(), value.as_ptr() as *const c_char, value.len()), 1);
     }
 }
