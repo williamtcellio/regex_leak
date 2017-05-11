e8cb18e18bec03695dec14102201754a5d376a7c
william <william@tcell.io>
2017-05-11 14:11:18 +0200

    Put exported function in public module

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,31 @@
 extern crate regex;
 extern crate rand;
 
-use std::str;
-use std::os::raw::c_int;
-use regex::RegexBuilder;
-use rand::{Rng, weak_rng};
+pub mod matcher {
+    use std::str;
+    use std::os::raw::c_int;
+    use regex::RegexBuilder;
+    use rand::{Rng, weak_rng};
 
-
-// Counts how many of a specified number of random transformations of a string that matches a certain pattern, returns the number of matches or negative for error
-pub extern fn match_random_strings(num_strings: usize) -> c_int {
-    let pattern = r"(?:<(script|iframe|embed|frame|frameset|object|img|applet|body|html|style|layer|link|ilayer|meta|bgsound))";
-    let matcher = match RegexBuilder::new(pattern).case_insensitive(true).dot_matches_new_line(true).multi_line(true).build() {
-        Ok(regex) => regex,
-        Err(_) => return -1
-    };
-    let mut value = r"<script>alert(123)</script><div><frame>Second Div</frame></div></body></html>".as_bytes().to_owned();
-    let mut matches = 0;
-    let mut rng = weak_rng();
-    for _ in 0..num_strings {
-        rng.shuffle(&mut value);    // Should be ok as long as the string only contains ascii
-        matches += match str::from_utf8(&value) {
-            Ok(value_str) => if matcher.is_match(value_str) { 1 } else { 0 },
-            _ => return -2
+    // Counts how many of a specified number of random transformations of a string that matches a certain pattern, returns the number of matches or negative for error
+    pub extern fn match_random_strings(num_strings: usize) -> c_int {
+        let pattern = r"(?:<(script|iframe|embed|frame|frameset|object|img|applet|body|html|style|layer|link|ilayer|meta|bgsound))";
+        let matcher = match RegexBuilder::new(pattern).case_insensitive(true).dot_matches_new_line(true).multi_line(true).build() {
+            Ok(regex) => regex,
+            Err(_) => return -1
+        };
+        let mut value = r"<script>alert(123)</script><div><frame>Second Div</frame></div></body></html>".as_bytes().to_owned();
+        let mut matches = 0;
+        let mut rng = weak_rng();
+        for _ in 0..num_strings {
+            rng.shuffle(&mut value);    // Should be ok as long as the string only contains ascii
+            matches += match str::from_utf8(&value) {
+                Ok(value_str) => if matcher.is_match(value_str) { 1 } else { 0 },
+                _ => return -2
+            }
         }
+        matches
     }
-    matches
 }
 
 #[cfg(test)]
